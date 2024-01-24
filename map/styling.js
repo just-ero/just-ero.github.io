2c8754b388224de2f1e339e6a031952cdf2902ac
Ero <56401411+just-ero@users.noreply.github.com>
2024-01-24 12:08:56 +0100

    Update styling.js

--- map/styling.js
+++ map/styling.js
@@ -1,34 +1,10 @@
-$('<div/>', {
-  id: 'map',
-  height: '100vh'
-}).appendTo('body');
+$('head').prepend([
+  $('<meta/>', { charset: 'utf-8' }),
+  $('<link/>', { rel: 'icon', href: 'assets/icon.png' }),
+  $('<link/>', { rel: 'stylesheet', href: '../global.css' }),
+  $('<link/>', { rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css' })
+]);
 
-$('<meta/>', {
-  charset: 'utf-8'
-}).appendTo('head');
-
-$('<link/>', {
-  rel: 'icon',
-  href: 'assets/icon.png'
-}).appendTo('head');
-
-$('<link/>', {
-  rel: 'stylesheet',
-  href: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css'
-}).appendTo('head');
-
-$('body').css({
-  height: '100%',
-  margin: 0
-});
-
-$('#map').css({
-  'font-size': '18pt',
-  'font-family': '"MapFont", sans-serif',
-  'line-height': 1.2,
-  'background': '#FFFFFF'
-});
-
-$('p').css({
-  margin: 0
-});
+$('body').prepend([
+  $('<div/>', { id: 'map', height: '100vh' })
+]);
